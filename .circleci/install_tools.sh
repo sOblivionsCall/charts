b7894d956cd30fb1d5201fd46fcdc99edf18de1c
sOblivionsCall <spencerhannah@gmail.com>
2021-03-27 20:39:11 -0500

    again'

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -2,14 +2,14 @@
 
 set -o errexit
 
-readonly HELM_VERSION=3.5.2
+# readonly HELM_VERSION=3.5.2
 readonly CHART_RELEASER_VERSION=0.1.4
 
 echo "Installing Helm..."
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-helm init --client-only
+# helm init --client-only
 
 echo "Installing chart-releaser..."
 curl -LO "https://github.com/helm/chart-releaser/releases/download/v${CHART_RELEASER_VERSION}/chart-releaser_${CHART_RELEASER_VERSION}_Linux_x86_64.tar.gz"
